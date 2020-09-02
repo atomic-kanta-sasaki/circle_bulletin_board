49b2893e74275afc50d237a4968978d6131a5cfa
kanta_sasaki <k_padbjw@icloud.com>
2020-09-02 12:33:46 +0900

    create stepper

--- circle_bulletin_board_frontend/src/EntryPage.js
+++ circle_bulletin_board_frontend/src/EntryPage.js
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 import PrimaryButton from './Button';
 import TopBar from './TopBar';
 import TextFieldComponent from './TextFiled';
-import Album from './Album'
-import Card from './Card'
-import ProgressBar from './ProgressBar'
+import Album from './Album';
+import Card from './Card';
+import ProgressBar from './ProgressBar';
+import Stepper from './Stepper';
 
 const Entry = styled.h1``;
 
@@ -21,12 +22,14 @@ class EntryPage extends Component {
                 <Entry>
                     コンポーネント一覧を示す
                 </Entry>
+                <ProgressBar />
+                <Stepper />
                 <TopBar />
                 <PrimaryButton />
                 <TextFieldComponent />
                 <Album />
                 <Card />
-                <ProgressBar />
+
             </div>
 
         )
