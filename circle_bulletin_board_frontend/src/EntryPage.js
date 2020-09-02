3427cc5e1c008c9042346ecaaf2897c50cfaf219
kanta_sasaki <k_padbjw@icloud.com>
2020-09-02 12:37:30 +0900

    translate file

--- circle_bulletin_board_frontend/src/EntryPage.js
+++ circle_bulletin_board_frontend/src/EntryPage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import PrimaryButton from './Button';
-import TopBar from './TopBar';
-import TextFieldComponent from './TextFiled';
-import Album from './Album';
-import Card from './Card';
-import ProgressBar from './ProgressBar';
-import Stepper from './Stepper';
+import PrimaryButton from './component/Button';
+import TopBar from './component/TopBar';
+import TextFieldComponent from './component/TextFiled';
+import Album from './component/Album';
+import Card from './component/Card';
+import ProgressBar from './component/ProgressBar';
+import Stepper from './component/Stepper';
 
 const Entry = styled.h1``;
 
