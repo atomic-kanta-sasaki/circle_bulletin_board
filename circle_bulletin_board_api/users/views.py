4bb9cccd0632e851dd7dc4b318cfa1f4154bc7c7
kanta_sasaki <k_padbjw@icloud.com>
2020-09-05 15:17:01 +0900

    add raw query

--- circle_bulletin_board_api/users/views.py
+++ circle_bulletin_board_api/users/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from .models import Users
 from .serializers import Usererializer
+from django.db import connection
 
 class UserListView(generics.ListAPIView):
-    queryset = Users.objects.all()
+    queryset = Users.objects.raw('SELECT * FROM users WHERE id=2')
     serializer_class = Usererializer
